4f8bd63d5aa3b68f1a256a16a1d5f1fc24338da5
ImFlexo <97156492+ImFlexo@users.noreply.github.com>
2022-12-26 15:32:14 +0300

    Add files via upload

---
+++ src/student_selection_for_vaccine/Main.java
@@ -0,0 +1,46 @@
+
+package student_selection_for_vaccine;
+
+import java.util.Scanner;
+public class Main {
+
+  
+    public static void main(String[] args) {
+        
+        System.out.print("Enter number of students: ");
+        Scanner sc = new Scanner(System.in);
+        int numOfStudents = sc.nextInt();
+        
+        String [] students = new String [numOfStudents];
+        int [] age = new int[numOfStudents];
+        
+        Scanner scName = new Scanner(System.in);
+        Scanner scAge = new Scanner(System.in);
+        
+        for(int i = 0; i < numOfStudents; i++){
+            System.out.print("Enter student name: ");
+            students[i] = scName.nextLine();
+            System.out.print("Enter student age: ");
+            age[i] = scAge.nextInt();
+        
+        }
+        
+        for(int i = 0; i < numOfStudents; i++ ){
+        if(age[i] >= 15){
+            System.out.println(students[i] + " can receive a vaccine.");
+            System.out.println(students[i]+" is " +age[i]+" years old!");
+        }
+        else if(age[i] <= 15 && age[i] >= 0){
+        System.out.println(students[i] + " can't receive a vaccine.");
+            System.out.println("because "+ students[i]+ " is only "+age[i]+" years old!");
+        
+        }
+        else    {
+        
+            System.out.println("invalid age!");
+            
+        }
+        }
+    }
+    
+}
